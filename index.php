720aded5d3386999b3103090432211cabf67ca40
lazzaric92 <lazzaric_92@hotmail.it>
2024-07-01 17:48:35 +0200

    added axios

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
         
     </div>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.2/axios.min.js" integrity="sha512-JSCFHhKDilTRRXe9ak/FJ28dcpOJxzQaCd3Xg8MyF6XFjODhy/YMCM8HW0TFDckNHWUewW+kfvhin43hKtJxAw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="./js/script.js"></script>
 </body>
